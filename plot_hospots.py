1426dd57d9eee0031f81d821d6ffc42dd09af2f5
Diego Mano Dellamula <dellamul@ENAC-SSIE-UC012.intranet.epfl.ch>
2025-12-05 13:00:35 +0100

    plots

--- plot_hospots.py
+++ plot_hospots.py
@@ -3,42 +3,7 @@ from matplotlib.colors import ListedColormap
 from pixel import * 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-pixelated_image = np.kron(small_image, np.ones((h_step, w_step, 1)))
-data = np.loadtxt("temperature_matrixe.csv", delimiter=",")
-
-def mean_filter_2d(arr, k=5):
-  
-    if k % 2 == 0:
-        raise ValueError("k doit être impair")
-    pad = k // 2
-
-    arr_p = np.pad(arr, pad_width=pad, mode='edge')
-
-    kernel = np.ones((k, k), dtype=float) / (k * k)
-
-    cumsum_h = np.cumsum(arr_p, axis=1)
-
-    h = cumsum_h[:, k:] - cumsum_h[:, :-k]
-    h = h / k
-
-    h_p = np.pad(h, ((pad, pad), (0, 0)), mode='edge')
-
-    cumsum_v = np.cumsum(h_p, axis=0)
-    v = cumsum_v[k:, :] - cumsum_v[:-k, :]
-    v = v / k
-
-    return v
-
-smoothed = mean_filter_2d(data, k=7)
-smoothed_rot = np.rot90(smoothed,2)
-fliped = np.fliplr(smoothed_rot)
-plt.imshow(fliped, cmap="inferno", origin="lower")
-plt.colorbar(label="Température (K)")
-
-plt.title("Carte de température (moyenne locale k=5)")
-plt.show()
+from plot_tempmap import *
 
 # On repart de ta variable 'fliped' déjà calculée
 hot_threshold = 355.0  # K
