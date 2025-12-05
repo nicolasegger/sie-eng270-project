7555dc52585174427e10a6b5d84512b0c10ca9c4
Nicolas Matthieu Egger <nmegger@ENAC-SSIE-UC001.intranet.epfl.ch>
2025-12-05 12:58:01 +0100

    .

--- app.sh
+++ app.sh
@@ -1,2 +1,7 @@
-python3 ~/Desktop/myfiles/CMT/pixel.py epfl_sat.jpg
+#python3 ~src./pixel.py data./epfl_sat.jpg
 
+python3 src/pixel.py src/classify.py data/epfl_sat.jpg
+gcc gcc src/lire_fichier.c -o src/lire_fichier
+./src/lire_fichier data/albedo_matrix.csv data/emissivity_matrix.csv data/temperature_matrix.csv
+
+python3 src/plot_tempmap.py data/temperature_matrix
---
+++ bin/lire_fichier1.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define ROWS 100
+#define COLS 100
+#define MAX_LINE_LENGTH 4096
+
+
+void lire_csv(const char *filename, double matrix[ROWS][COLS]) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error");// error if file doesn't open
+        exit(1);
+    }
+
+    char line[MAX_LINE_LENGTH];
+    int row = 0;
+
+    while (fgets(line, sizeof(line), file) && row < ROWS) {
+        char *token = strtok(line, ","); //cut the line at every ","
+        int col = 0;
+
+        while (token != NULL && col < COLS) {
+            matrix[row][col] = atof(token); //convert the .csv value into a number (double)
+            token = strtok(NULL, ",");
+            col++;
+        }
+        row++;
+    }
+
+    fclose(file);
+    
+}
+
+//initialisation des variables
+double sigma = 5.67e-8;
+double rayonnement = 1000;
+double temperature_surface = 0;
+
+//loi de stefan
+
+double stefan(double albedo, double emissivity, double sigma, double rayonnement) {
+    // Stefan-Boltzmann law: T = [ ( (1 - albedo) * rayonnement ) / (emissivity * sigma) ]^(1/4)
+    double temperature_surface = pow(((1.0 - albedo) * rayonnement) /(0.95 * sigma), (1.0/4.0));
+    return temperature_surface;
+}
+
+double tempMatrix[ROWS][COLS];
+int main() {
+    double albedomatrix[ROWS][COLS];
+    double emissivitymatrix[ROWS][COLS];
+
+    // read 2 files
+    lire_csv("albedo_matrix.csv", albedomatrix);
+    lire_csv("emissivity_matrix.csv", emissivitymatrix);
+    
+    // calculs
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            double albedo = albedomatrix[i][j];
+            double emissivity = emissivitymatrix[i][j];
+            double T = stefan(albedo, emissivity, sigma, rayonnement);
+            tempMatrix[i][j] = T;
+        }
+    }
+
+    FILE *fp = fopen("temperature_matrixe.csv", "w");
+        if (fp == NULL) {
+            perror("Erreur lors de la création du fichier CSV");
+            return 1;
+        }
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                fprintf(fp, "%.2f", tempMatrix[i][j]);
+                if (j < COLS - 1) {
+                    fprintf(fp, ",");
+                }
+            }
+            fprintf(fp, "\n");
+        }
+
+        fclose(fp);
+        printf("Fichier CSV créé : temperature_matrix.csv\n");
+
+
+/*
+    // Exemple d'affichage de la matrice
+    printf("Matrice lue (première ligne):\n");
+    for (int j = 0; j < COLS; j++) {
+        printf("%.10f ", matrix[0][j]);
+    }
+    printf("\n");
+*/
+    return 0;
+}
+
+
